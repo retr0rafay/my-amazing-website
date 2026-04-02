eed3370659960a3f76da06799ddad8071da25c93
Rafay Syed <rafaysyeda@live.com>
2026-04-02 10:48:26 -0400

    Adding new blog post

--- src/blog/articles/index.js
+++ src/blog/articles/index.js
@@ -5,8 +5,14 @@
 import HelloWorldArticle, { meta as helloWorldMeta } from './hello-world'
 import SoftwareEngineersAgeOfAIArticle, { meta as softwareEngineersMeta } from './software-engineers-age-of-ai'
 import JuniorSoftwareEngineersInTheAIEraArticle, { meta as juniorEngineersMeta } from './junior-software-engineers-in-the-ai-era'
+import ThingsILearnedInMarch2026Article, { meta as thingsILearnedInMarch2026Meta } from './things-i-learned-in-march-2026'
 
 export const articles = [
+  {
+    slug: thingsILearnedInMarch2026Meta.slug,
+    meta: thingsILearnedInMarch2026Meta,
+    Component: ThingsILearnedInMarch2026Article,
+  },
   {
     slug: juniorEngineersMeta.slug,
     meta: juniorEngineersMeta,
